63b05b375785a2436f316512b296f461725e747a
Jean-Daniel Franel <jd@franel.me>
2026-06-05 12:49:15 +0200

    test: move cypress test in the appropriate file

--- tests/cypress/template/concept.cy.js
+++ tests/cypress/template/concept.cy.js
@@ -155,19 +155,6 @@ describe('Concept page', () => {
     // Check that mapping property title is overridden correctly
     cy.get('.prop-mapping .property-label').eq(0).should('have.attr', 'title').and('contain', 'Exactly matching classes in another vocabulary.')
   })
-  it('overrides vocabulary property labels', () => {
-    // Go to "Carp" concept page in vocab with property label overrides
-    cy.visit('/conceptPropertyLabels/en/')
-    // Loop through all .row.property and if the element has a .property-value ul li with text "Overridden property value", check the label and title of the .property-label for this row
-    cy.get('.row.property').each($row => {
-      cy.wrap($row).find('.property-value ul li').each($li => {
-        if ($li.text().includes('Overridden property value')) {
-          cy.wrap($row).find('.property-label h2').eq(0).invoke('text').should('include', 'Vocabulary Property')
-          cy.wrap($row).find('.property-label h2').eq(0).should('have.attr', 'data-title').and('contain', 'Vocabulary Property description')
-        }
-      })
-    })
-  })
   it('contains SKOS XL information for concept prefLabel', () => {
     cy.visit('/yso/en/page/p4625?clang=se') // go to "bronsaáigi" concept page ('Bronze Age' in Northern Sami)
 
